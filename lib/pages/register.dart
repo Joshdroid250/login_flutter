1702cb575685a64fc499a90fa205ed68e1adc24c
Joshdroid <gutierrez25g@gmail.com>
2023-06-21 20:09:26 -0600

    ListView Form for RegisterPag

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -7,7 +7,7 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: myAppBar(),
+      appBar: myAppBar(context),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -18,18 +18,21 @@ class RegisterPage extends StatelessWidget {
   }
 }
 
-AppBar myAppBar() {
+AppBar myAppBar(BuildContext context) {
   return AppBar(
     backgroundColor: Colors.transparent, // Hace que el fondo del AppBar sea transparente
     elevation: 1, // Elimina la sombra del AppBar
     flexibleSpace: Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         image: DecorationImage(
           image: NetworkImage(
               "https://www.pixelstalk.net/wp-content/uploads/2016/04/Abstract-minimalist-wallpaper-HD-desktop-download.png"),
           fit: BoxFit.cover,
         ),
       ),
+    ),
+    leading: BackButton(
+      onPressed: () => Navigator.of(context).pop(),
     ),
   );
 }
@@ -92,10 +95,152 @@ Widget hiBox() {
   );
 }
 
+Widget Form() {
+  return ListView(
+    children: [
+      Flexible(
+          child: Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 10.0, bottom: 0.0),
+            child: const Text(
+              "Nombre",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontFamily: 'ProductSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15),
+            ),
+          )),
+      Flexible(
+          child: Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            margin: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: 'Enter your Name')),
+          )),
+      Flexible(
+          child: Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 10.0, bottom: 0.0),
+            child: const Text(
+              "Mail",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontFamily: 'ProductSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15),
+            ),
+          )),
+      Flexible(
+          child: Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            margin: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Mail',
+                    hintText: 'Enter your Mail')),
+          )),
+      Flexible(
+          child: Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 10.0, bottom: 0.0),
+            child: const Text(
+              "Password",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontFamily: 'ProductSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15),
+            ),
+          )),
+      Flexible(
+          child: Container(
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            margin: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your Password')),
+          ))
+    ],
+  );
+}
+
+Widget RegisterButtons(){
+  return Container(
+    padding: EdgeInsets.only(top: 20),
+    child: Column(
+      children: [
+        Flexible(
+            child: Container(
+              height: 100,
+              width: 500,
+              margin: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+              child: TextButton(
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    primary: Colors.blueAccent),
+                child: const Text(
+                  "Register",
+                  style: TextStyle(fontFamily: "ProductSans", color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            )),
+        Flexible(
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 10),
+              child: const Text(
+                "or",
+                style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontFamily: 'ProductSans',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+            )),
+        Flexible(
+            child: Container(
+              height: 100,
+              width: 500,
+              margin: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.blueAccent),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    primary: Colors.white),
+                child: const Text(
+                  "Google Register",
+                  style: TextStyle(
+                      fontFamily: "ProductSans", color: Colors.blueAccent),
+                ),
+                onPressed: () {},
+              ),
+            )),
+      ],
+    ),
+  );
+}
+
 Widget Contenedor() {
   return Container(
     child: Column(
-      children: [SizedBox(height: 130, child: Flexible(child: hiBox()))],
+      children: [
+        SizedBox(height: 130, child: Flexible(child: hiBox())),
+        SizedBox(height: 200, child: Flexible(child: Form())),
+        SizedBox(height: 200, child: Flexible(child: RegisterButtons()))],
     ),
   );
 }
