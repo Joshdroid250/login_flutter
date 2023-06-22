dad4437d87ba362dfb5deccb1ec4a4f88742a4e7
Joshdroid <gutierrez25g@gmail.com>
2023-06-21 19:36:55 -0600

    Register hiWidget

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -34,10 +34,68 @@ AppBar myAppBar() {
   );
 }
 
+Widget Welcome() {
+  return Container(
+    margin: EdgeInsets.only(left: 10.0),
+    alignment: Alignment.topLeft,
+    child: Column(
+      children: <Widget>[
+        Container(
+          child: const Text(
+            "Welcome  to Fashion Daily",
+            style: TextStyle(
+                color: Colors.grey,
+                fontFamily: 'ProductSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 15),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 0.0, top: 20.0),
+          alignment: Alignment.centerLeft,
+          child: const Text(
+            "Register",
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'ProductSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 40),
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget infoHelp() {
+  return Container(
+    margin: const EdgeInsets.only(bottom: 80.0, right: 20.0),
+    alignment: Alignment.centerRight,
+    child: const Text(
+      "Help",
+      style: TextStyle(
+          color: Colors.blueAccent,
+          fontFamily: 'ProductSans',
+          fontWeight: FontWeight.bold,
+          fontSize: 15),
+    ),
+  );
+}
+
+Widget hiBox() {
+  return Container(
+    padding: EdgeInsets.only(top: 30),
+    child: GridView.count(
+      crossAxisCount: 2,
+      children: <Widget>[Welcome(), infoHelp()],
+    ),
+  );
+}
+
 Widget Contenedor() {
   return Container(
     child: Column(
-      children: [],
+      children: [SizedBox(height: 130, child: Flexible(child: hiBox()))],
     ),
   );
 }
